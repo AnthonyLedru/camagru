a0b2ef6885b70351316e9ef51711cf860e5fdd4a
Anthony <anthony@MacBook-Pro-de-Anthony.local>
2018-04-17 22:41:08 +0200

    mr proprer

--- include/autoload.include.php
+++ include/autoload.include.php
@@ -1,7 +1,6 @@
 <?php
 
 spl_autoload_register(function($className) {
-
     if (file_exists(__DIR__ . "/../class/" . $className . ".class.php"))
             require_once(__DIR__ . "/../class/" . $className . ".class.php");
 });
